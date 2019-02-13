01f56aadbf9e6a417b2e45640091838b575cf512
Rohit <rohit.sharma@wisc.edu>
2019-02-12 19:44:57 -0600

    Generate labeled image

--- Assignments/HW2/src/generateLabeledImage.m
+++ Assignments/HW2/src/generateLabeledImage.m
@@ -1,1 +1,12 @@
 function labeled_img = generateLabeledImage(gray_img, threshold)
+fh = figure();
+imshow(gray_img);
+
+bw_img = im2bw(gray_img, threshold);
+imshow(bw_img);
+
+labeled_img = bwlabel(bw_img);
+imshow(labeled_img);
+
+%rgb_img = label2rgb(labeled_img, 'jet', 'k'); 
+%imshow(rgb_img); % now much better!
