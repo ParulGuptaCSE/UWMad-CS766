d0f6d055c813ca6876cbb5edeeb44292b1f864df
Rohit <rohit.sharma@wisc.edu>
2019-03-09 16:54:01 -0600

    Bug in show correspondance, add width instead of height to the right img

--- Assignments/HW4/src/showCorrespondence.m
+++ Assignments/HW4/src/showCorrespondence.m
@@ -7,7 +7,7 @@ function result_img = ...
     
     % Shift the x coordinates of warped image by the width of original
     % image to plot
-    orig_img_wid = size(orig_img, 1);
+    orig_img_wid = size(orig_img, 2);
     dest_pts_nx2(:, 1) = dest_pts_nx2(:, 1) + orig_img_wid;
     for idx = 1 : size(src_pts_nx2, 1)
         line([src_pts_nx2(idx, 1) dest_pts_nx2(idx, 1)], [src_pts_nx2(idx, 2) dest_pts_nx2(idx, 2)], 'lineWidth', 1, 'Color', 'r');
