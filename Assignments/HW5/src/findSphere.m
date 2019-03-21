f2250c0be7b65989df86ca13f3a439b71d33cc90
Rohit <rohit.sharma@wisc.edu>
2019-03-20 19:23:36 -0500

    Computing sphere's center and radius done

--- Assignments/HW5/src/findSphere.m
+++ Assignments/HW5/src/findSphere.m
@@ -1,1 +1,7 @@
 function [center, radius] = findSphere(img)
+    bw_img = im2bw(img, 0.0001);
+    img_props = regionprops(bw_img, 'Area', 'Centroid');
+    
+    center = img_props.Centroid;
+    radius = sqrt(img_props.Area / pi);
+end
