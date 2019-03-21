4c7cade501026676cf970b5234d4fadbeb93f04e
Rohit <rohit.sharma@wisc.edu>
2019-03-20 21:06:49 -0500

    Finished computing the magnitude and direction of light source

--- Assignments/HW5/src/README.md
+++ Assignments/HW5/src/README.md
@@ -2,4 +2,5 @@
 ## Homework 5 - `Photometric Stereo`
 Recovering shape, surface normal and reflectance of an object using photometric stereo.
 ### Challenge 1b:
-
+**Formula for Normal vector:**
+**Why is the direction of light source the normal with the brightest spot?**
--- Assignments/HW5/src/computeLightDirections.m
+++ Assignments/HW5/src/computeLightDirections.m
@@ -1,3 +1,21 @@
 function light_dirs_5x3 = computeLightDirections(center, radius, img_cell)
-    
+    light_dirs_5x3 = zeros(5, 3);
+    for i = 1 : 5
+        img = img_cell{i};
+        [max_intensity, idx] = max(img(:));
+        [max_int_r, max_int_c] = ind2sub(size(img), idx);
+        
+        dir_x = max_int_r - center(1);
+        dir_y = max_int_c - center(2);
+        dir_z = sqrt(radius^2 - (dir_x^2 + dir_y^2));
+        
+        % Let the magnitude be the max intensity
+        dir_x = dir_x / (dir_x^2 + dir_y^2 + dir_z^2) * max_intensity;
+        dir_y = dir_y / (dir_x^2 + dir_y^2 + dir_z^2) * max_intensity;
+        dir_z = dir_z / (dir_x^2 + dir_y^2 + dir_z^2) * max_intensity;
+        
+        light_dirs_5x3(i, 1) = dir_x;
+        light_dirs_5x3(i, 2) = dir_y;
+        light_dirs_5x3(i, 3) = dir_z;
+    end
 end
