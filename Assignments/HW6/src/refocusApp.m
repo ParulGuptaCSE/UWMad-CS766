ab1a411d67d22a21e5d2192025c54063c412541b
Rohit <rohit.sharma@wisc.edu>
2019-04-05 17:08:57 -0500

    Undo round in one line as round only returns one output value

--- Assignments/HW6/src/refocusApp.m
+++ Assignments/HW6/src/refocusApp.m
@@ -3,11 +3,13 @@ function refocusApp(rgb_stack, depth_map)
     
     figure, imshow(rgb_stack(:, :, 1:3));
     
-    [y, x] = round(ginput(1));
+    [y, x] = ginput(1);
+    x = round(x); y = round(y);
     while y > 0 && y < wid && x > 0 && x < ht
         best_idx = depth_map(x, y);
         imshow(rgb_stack(:, :, 3*(best_idx-1)+1 : 3*best_idx));
         
-        [y, x] = round(ginput(1));
+        [y, x] = ginput(1);
+        x = round(x); y = round(y);
     end
 end
