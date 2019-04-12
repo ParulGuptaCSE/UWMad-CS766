5a83533f3e7f1b451fd7135093652409bc7f444c
Rohit <rohit.sharma@wisc.edu>
2019-04-11 21:53:52 -0500

    Increased averaging window size. Changed padding width to 40

--- Assignments/HW6/src/refocusApp.m
+++ Assignments/HW6/src/refocusApp.m
@@ -1,15 +1,15 @@
 function refocusApp(rgb_stack, depth_map)
     [ht, wid] = size(depth_map);
-    padding = 50;
+    padding = 40;
     
-    canvas = padarray(rgb_stack(:, :, 1:3), [padding padding], 'both');
+    canvas = padarray(rgb_stack(:, :, 1:3), [padding padding], 255, 'both');
     fig = figure('Name', 'Refocusing App'); imshow(canvas);
     
     [y, x] = ginput(1);
     x = round(x) - padding; y = round(y) - padding;
     while y > 0 && y < wid && x > 0 && x < ht
         best_idx = depth_map(x, y);
-        canvas = padarray(rgb_stack(:, :, 3*(best_idx-1)+1 : 3*best_idx), [padding padding], 'both');
+        canvas = padarray(rgb_stack(:, :, 3*(best_idx-1)+1 : 3*best_idx), [padding padding], 255, 'both');
         imshow(canvas);
         
         [y, x] = ginput(1);
--- Assignments/HW6/src/runHw6.m
+++ Assignments/HW6/src/runHw6.m
@@ -51,7 +51,7 @@ focal_stack_dir = 'stack';
 % gray_stack is an mxnxk matrix.
 
 % Specify the (half) window size used for focus measure computation
-half_window_size = 5; 
+half_window_size = 15; 
 
 % Generate an index map, here we will only use the gray-scale images
 index_map = generateIndexMap(gray_stack, half_window_size);
