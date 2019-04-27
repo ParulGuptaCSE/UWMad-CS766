9a113f40853216e6ac92cb10cae28823b7d6535a
Rohit <rohit.sharma@wisc.edu>
2019-04-26 19:15:12 -0500

    Made object tracking faster by searching in just the window

--- Assignments/HW7/src/trackingTester.m
+++ Assignments/HW7/src/trackingTester.m
@@ -1,39 +1,51 @@
 function trackingTester(data_params, tracking_params)
     img = imread(fullfile(data_params.data_dir, data_params.genFname(data_params.frame_ids(1))));
-    figure, imshow(img);
+    % figure, imshow(img);
     
     src_template = img(tracking_params.rect(2) : tracking_params.rect(2) + tracking_params.rect(4), tracking_params.rect(1) : tracking_params.rect(1) + tracking_params.rect(3), :);
-    figure, imshow(src_template);
+    % figure, imshow(src_template);
     
     [src_temp_ind, src_temp_map] = rgb2ind(src_template, 32);
     src_temp_hist = histc(src_temp_ind(:), 1:tracking_params.bin_n);
     
+    obj_rect = tracking_params.rect;
+    
     for i = data_params.frame_ids
         dest_search_img = imread(fullfile(data_params.data_dir, data_params.genFname(i)));
-        dest_img_r = dest_search_img(:, :, 1);
-        dest_img_g = dest_search_img(:, :, 2);
-        dest_img_b = dest_search_img(:, :, 3);
+        [ht, wid, ~] = size(dest_search_img);
+        dest_x_strt = max(1, obj_rect(1) - tracking_params.search_half_window_size);
+        dest_x_end = min(wid, obj_rect(1) + obj_rect(3) + tracking_params.search_half_window_size);
+        dest_y_strt = max(1, obj_rect(2) - tracking_params.search_half_window_size);
+        dest_y_end = min(ht, obj_rect(2) + obj_rect(4) + tracking_params.search_half_window_size);
         
-        dest_windows_r = im2col(dest_img_r, [tracking_params.rect(4), tracking_params.rect(3)]);
-        dest_windows_g = im2col(dest_img_g, [tracking_params.rect(4), tracking_params.rect(3)]);
-        dest_windows_b = im2col(dest_img_b, [tracking_params.rect(4), tracking_params.rect(3)]);
-        similarity = zeros(size(dest_windows_r, 2), 1);
-        for j = 1 : size(dest_windows_r, 2)
-            curr_window_r = reshape(dest_windows_r(:, j), [tracking_params.rect(4), tracking_params.rect(3)]);
-            curr_window_g = reshape(dest_windows_g(:, j), [tracking_params.rect(4), tracking_params.rect(3)]);
-            curr_window_b = reshape(dest_windows_b(:, j), [tracking_params.rect(4), tracking_params.rect(3)]);
-            curr_window = cat(3, curr_window_r, curr_window_g, curr_window_b);
+        search_window = dest_search_img(dest_y_strt:dest_y_end, dest_x_strt:dest_x_end, :);
+        % figure, imshow(search_window);
+        
+        search_windows(:, :, 1) = im2col(search_window(:, :, 1), [obj_rect(4), obj_rect(3)]);
+        search_windows(:, :, 2) = im2col(search_window(:, :, 2), [obj_rect(4), obj_rect(3)]);
+        search_windows(:, :, 3) = im2col(search_window(:, :, 3), [obj_rect(4), obj_rect(3)]);
+        similarity = zeros(size(search_window, 2), 1);
+        for j = 1 : size(search_windows, 2)
             % figure, imshow(curr_window);
-            curr_wind_ind = rgb2ind(curr_window, src_temp_map);
+            curr_wind_ind = rgb2ind(search_windows(:, j, :), src_temp_map);
             curr_wind_hist = histc(curr_wind_ind(:), 1:tracking_params.bin_n);
             
             similarity(j) = norm(src_temp_hist - curr_wind_hist);
         end
         [~, idx] = min(similarity);
-        window_r = reshape(dest_windows_r(:, idx), [tracking_params.rect(4), tracking_params.rect(3)]);
-        window_g = reshape(dest_windows_g(:, idx), [tracking_params.rect(4), tracking_params.rect(3)]);
-        window_b = reshape(dest_windows_b(:, idx), [tracking_params.rect(4), tracking_params.rect(3)]);
-        window = cat(3, window_r, window_g, window_b);
-        figure, imshow(window);
+        
+        % TODO: write comments for this formula
+        denom = size(search_window, 1) - obj_rect(4) + 1;
+        rect_x = ceil(idx / denom) + dest_x_strt;
+        rect_y = mod(idx, denom) + dest_y_strt;
+        if rect_y == dest_y_strt
+            rect_y = denom + dest_y_strt;
+        end
+        
+        obj_rect = [rect_x, rect_y, obj_rect(3), obj_rect(4)];
+        res = drawBox(dest_search_img, obj_rect, [0, 255, 0], 1);
+        % fig = figure; imshow(res);
+        imwrite(res, fullfile(data_params.out_dir, data_params.genFname(i)));
+        % close(fig);
     end
 end
