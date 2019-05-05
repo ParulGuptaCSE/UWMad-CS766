9d1e7273fb3546d5567a79709587cfab585eb2be
Rohit <rohit.sharma@wisc.edu>
2019-05-05 15:02:46 -0500

    Vectorized quiver plotting part to make the code run much faster.

--- Assignments/HW7/src/computeFlow.m
+++ Assignments/HW7/src/computeFlow.m
@@ -1,7 +1,7 @@
 function result = computeFlow(img1, img2, win_radius, template_radius, grid_MN)
     [ht, wid] = size(img1);
-    opticalFlow = zeros(grid_MN(1), grid_MN(2), 2);
-    
+    flow_pos = [];
+    flow_vec = [];
     c = 1;
     for x = round(wid / (grid_MN(2) + 1)) + 1 : round(wid / (grid_MN(2) + 1)) : wid - round(wid / (grid_MN(2) + 1))
         r = 1;
@@ -25,22 +25,18 @@ function result = computeFlow(img1, img2, win_radius, template_radius, grid_MN)
             
             % Transform the coordinates to original frame of reference
             % while finding the optical flow vector direction
-            opticalFlow(y, x, :) = [x_match - (src_x_strt - dest_x_strt + 1), y_match - (src_y_strt - dest_y_strt + 1)];
+            flow_pos = [flow_pos; y, x];
+            flow_vec = [flow_vec; x_match - (src_x_strt - dest_x_strt + 1), y_match - (src_y_strt - dest_y_strt + 1)];
             r = r + 1;
         end
         c = c + 1;
     end
     
     % Draw the needle plot on img1.
-    % TODO: This takes very long. Optimize it?
     fig = figure;
     imshow(img1);
     hold on;
-    for y = round(ht / (grid_MN(1) + 1)) + 1 : round(ht / (grid_MN(1) + 1)) : ht
-        for x = round(wid / (grid_MN(2) + 1)) + 1 : round(wid / (grid_MN(2) + 1)) : wid - round(wid / (grid_MN(2) + 1))
-            quiver(x, y, opticalFlow(y, x, 1), opticalFlow(y, x, 2), 'Color', 'y', 'MaxHeadSize', 1);
-        end
-    end
+    quiver(flow_pos(:, 2), flow_pos(:, 1), flow_vec(:, 1), flow_vec(:, 2), 0, 'filed', 'r');
     result = saveAnnotatedImg(fig);
     close(fig);
 end
